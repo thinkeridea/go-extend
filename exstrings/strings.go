aef8c7cf95283c77a66d38be77d2d1a7e11fc06a
qiyin <qiyin@thinkeridea.com>
2018-12-27 09:01:34 +0800

    收集反转字符串的方法

---
+++ exstrings/strings.go
@@ -0,0 +1,25 @@
+// Copyright (C) 2018  Qi Yin <qiyin@thinkeridea.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package exstrings
+
+// Reverse 反转字符串，通过 https://golang.org/doc/code.html#Library 收集
+func Reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}

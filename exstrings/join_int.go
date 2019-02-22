18a1232df8040fa860325a099b8b408409f95310
qiyin <qiyin@thinkeridea.com>
2019-02-22 19:28:06 +0800

    增加连接各种类型数值切片的方法

---
+++ exstrings/join_int.go
@@ -0,0 +1,164 @@
+// Copyright (C) 2018  Qi Yin <qiyin@thinkeridea.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package exstrings
+
+import (
+	"strconv"
+
+	"github.com/thinkeridea/go-extend/pool"
+)
+
+var buffPool = pool.GetBuff64()
+
+// JoinInts 使用 sep 连接 []int 并返回连接的字符串
+func JoinInts(i []int, sep string) string {
+	buf := buffPool.Get()
+	for _, v := range i {
+		if buf.Len() > 0 {
+			buf.WriteString(sep)
+		}
+		buf.WriteString(strconv.FormatInt(int64(v), 10))
+	}
+
+	buffPool.Put(buf)
+	return buf.String()
+}
+
+// JoinInt8s 使用 sep 连接 []int8 并返回连接的字符串
+func JoinInt8s(i []int8, sep string) string {
+	buf := buffPool.Get()
+	for _, v := range i {
+		if buf.Len() > 0 {
+			buf.WriteString(sep)
+		}
+		buf.WriteString(strconv.FormatInt(int64(v), 10))
+	}
+
+	buffPool.Put(buf)
+	return buf.String()
+}
+
+// JoinInt16s 使用 sep 连接 []int16 并返回连接的字符串
+func JoinInt16s(i []int16, sep string) string {
+	buf := buffPool.Get()
+	for _, v := range i {
+		if buf.Len() > 0 {
+			buf.WriteString(sep)
+		}
+		buf.WriteString(strconv.FormatInt(int64(v), 10))
+	}
+
+	buffPool.Put(buf)
+	return buf.String()
+}
+
+// JoinInt32s 使用 sep 连接 []int32 并返回连接的字符串
+func JoinInt32s(i []int32, sep string) string {
+	buf := buffPool.Get()
+	for _, v := range i {
+		if buf.Len() > 0 {
+			buf.WriteString(sep)
+		}
+		buf.WriteString(strconv.FormatInt(int64(v), 10))
+	}
+
+	buffPool.Put(buf)
+	return buf.String()
+}
+
+// JoinInt64s 使用 sep 连接 []int64 并返回连接的字符串
+func JoinInt64s(i []int64, sep string) string {
+	buf := buffPool.Get()
+	for _, v := range i {
+		if buf.Len() > 0 {
+			buf.WriteString(sep)
+		}
+		buf.WriteString(strconv.FormatInt(v, 10))
+	}
+
+	buffPool.Put(buf)
+	return buf.String()
+}
+
+// JoinUInts 使用 sep 连接 []uint 并返回连接的字符串
+func JoinUints(i []uint, sep string) string {
+	buf := buffPool.Get()
+	for _, v := range i {
+		if buf.Len() > 0 {
+			buf.WriteString(sep)
+		}
+		buf.WriteString(strconv.FormatUint(uint64(v), 10))
+	}
+
+	buffPool.Put(buf)
+	return buf.String()
+}
+
+// JoinUInt8s 使用 sep 连接 []uint8 并返回连接的字符串
+func JoinUint8s(i []uint8, sep string) string {
+	buf := buffPool.Get()
+	for _, v := range i {
+		if buf.Len() > 0 {
+			buf.WriteString(sep)
+		}
+		buf.WriteString(strconv.FormatUint(uint64(v), 10))
+	}
+
+	buffPool.Put(buf)
+	return buf.String()
+}
+
+// JoinUInt16s 使用 sep 连接 []uint16 并返回连接的字符串
+func JoinUint16s(i []uint16, sep string) string {
+	buf := buffPool.Get()
+	for _, v := range i {
+		if buf.Len() > 0 {
+			buf.WriteString(sep)
+		}
+		buf.WriteString(strconv.FormatUint(uint64(v), 10))
+	}
+
+	buffPool.Put(buf)
+	return buf.String()
+}
+
+// JoinUInt32s 使用 sep 连接 []uint32 并返回连接的字符串
+func JoinUint32s(i []uint32, sep string) string {
+	buf := buffPool.Get()
+	for _, v := range i {
+		if buf.Len() > 0 {
+			buf.WriteString(sep)
+		}
+		buf.WriteString(strconv.FormatUint(uint64(v), 10))
+	}
+
+	buffPool.Put(buf)
+	return buf.String()
+}
+
+// JoinUInt64s 使用 sep 连接 []uint64 并返回连接的字符串
+func JoinUint64s(i []uint64, sep string) string {
+	buf := buffPool.Get()
+	for _, v := range i {
+		if buf.Len() > 0 {
+			buf.WriteString(sep)
+		}
+		buf.WriteString(strconv.FormatUint(v, 10))
+	}
+
+	buffPool.Put(buf)
+	return buf.String()
+}
---
+++ exstrings/join_int_test.go
@@ -0,0 +1,203 @@
+package exstrings
+
+import "testing"
+
+func TestJoinInts(t *testing.T) {
+	cases := []struct {
+		in       []int
+		sep, out string
+	}{
+		{[]int{1, 2, 3, 4, 5}, "", "12345"},
+		{[]int{1, 2, 3, 4, 5}, ",", "1,2,3,4,5"},
+		{[]int{1, 2, 3, 4, 5}, "}{", "1}{2}{3}{4}{5"},
+		{[]int{1, 2}, ",", "1,2"},
+		{[]int{1}, ",", "1"},
+		{[]int{}, ",", ""},
+	}
+	for _, c := range cases {
+		got := JoinInts(c.in, c.sep)
+		if got != c.out {
+			t.Errorf("JoinInts(%v, %q) == %q, out %q", c.in, c.sep, got, c.out)
+		}
+	}
+}
+
+func TestJoinInt8s(t *testing.T) {
+	cases := []struct {
+		in       []int8
+		sep, out string
+	}{
+		{[]int8{1, 2, 3, 4, 5}, "", "12345"},
+		{[]int8{1, 2, 3, 4, 5}, ",", "1,2,3,4,5"},
+		{[]int8{1, 2, 3, 4, 5}, "}{", "1}{2}{3}{4}{5"},
+		{[]int8{1, 2}, ",", "1,2"},
+		{[]int8{1}, ",", "1"},
+		{[]int8{}, ",", ""},
+	}
+	for _, c := range cases {
+		got := JoinInt8s(c.in, c.sep)
+		if got != c.out {
+			t.Errorf("JoinInt8s(%v, %q) == %q, out %q", c.in, c.sep, got, c.out)
+		}
+	}
+}
+
+func TestJoinInt16s(t *testing.T) {
+	cases := []struct {
+		in       []int16
+		sep, out string
+	}{
+		{[]int16{1, 2, 3, 4, 5}, "", "12345"},
+		{[]int16{1, 2, 3, 4, 5}, ",", "1,2,3,4,5"},
+		{[]int16{1, 2, 3, 4, 5}, "}{", "1}{2}{3}{4}{5"},
+		{[]int16{1, 2}, ",", "1,2"},
+		{[]int16{1}, ",", "1"},
+		{[]int16{}, ",", ""},
+	}
+	for _, c := range cases {
+		got := JoinInt16s(c.in, c.sep)
+		if got != c.out {
+			t.Errorf("JoinInt16s(%v, %q) == %q, out %q", c.in, c.sep, got, c.out)
+		}
+	}
+}
+
+func TestJoinInt32s(t *testing.T) {
+	cases := []struct {
+		in       []int32
+		sep, out string
+	}{
+		{[]int32{1, 2, 3, 4, 5}, "", "12345"},
+		{[]int32{1, 2, 3, 4, 5}, ",", "1,2,3,4,5"},
+		{[]int32{1, 2, 3, 4, 5}, "}{", "1}{2}{3}{4}{5"},
+		{[]int32{1, 2}, ",", "1,2"},
+		{[]int32{1}, ",", "1"},
+		{[]int32{}, ",", ""},
+	}
+	for _, c := range cases {
+		got := JoinInt32s(c.in, c.sep)
+		if got != c.out {
+			t.Errorf("JoinInt32s(%v, %q) == %q, out %q", c.in, c.sep, got, c.out)
+		}
+	}
+}
+
+func TestJoinInt64s(t *testing.T) {
+	cases := []struct {
+		in       []int64
+		sep, out string
+	}{
+		{[]int64{1, 2, 3, 4, 5}, "", "12345"},
+		{[]int64{1, 2, 3, 4, 5}, ",", "1,2,3,4,5"},
+		{[]int64{1, 2, 3, 4, 5}, "}{", "1}{2}{3}{4}{5"},
+		{[]int64{1, 2}, ",", "1,2"},
+		{[]int64{1}, ",", "1"},
+		{[]int64{}, ",", ""},
+	}
+	for _, c := range cases {
+		got := JoinInt64s(c.in, c.sep)
+		if got != c.out {
+			t.Errorf("JoinInt64s(%v, %q) == %q, out %q", c.in, c.sep, got, c.out)
+		}
+	}
+}
+
+func TestJoinUints(t *testing.T) {
+	cases := []struct {
+		in       []uint
+		sep, out string
+	}{
+		{[]uint{1, 2, 3, 4, 5}, "", "12345"},
+		{[]uint{1, 2, 3, 4, 5}, ",", "1,2,3,4,5"},
+		{[]uint{1, 2, 3, 4, 5}, "}{", "1}{2}{3}{4}{5"},
+		{[]uint{1, 2}, ",", "1,2"},
+		{[]uint{1}, ",", "1"},
+		{[]uint{}, ",", ""},
+	}
+	for _, c := range cases {
+		got := JoinUints(c.in, c.sep)
+		if got != c.out {
+			t.Errorf("JoinUints(%v, %q) == %q, out %q", c.in, c.sep, got, c.out)
+		}
+	}
+}
+
+func TestJoinUint8s(t *testing.T) {
+	cases := []struct {
+		in       []uint8
+		sep, out string
+	}{
+		{[]uint8{1, 2, 3, 4, 5}, "", "12345"},
+		{[]uint8{1, 2, 3, 4, 5}, ",", "1,2,3,4,5"},
+		{[]uint8{1, 2, 3, 4, 5}, "}{", "1}{2}{3}{4}{5"},
+		{[]uint8{1, 2}, ",", "1,2"},
+		{[]uint8{1}, ",", "1"},
+		{[]uint8{}, ",", ""},
+	}
+	for _, c := range cases {
+		got := JoinUint8s(c.in, c.sep)
+		if got != c.out {
+			t.Errorf("JoinUint8s(%v, %q) == %q, out %q", c.in, c.sep, got, c.out)
+		}
+	}
+}
+
+func TestJoinUint16s(t *testing.T) {
+	cases := []struct {
+		in       []uint16
+		sep, out string
+	}{
+		{[]uint16{1, 2, 3, 4, 5}, "", "12345"},
+		{[]uint16{1, 2, 3, 4, 5}, ",", "1,2,3,4,5"},
+		{[]uint16{1, 2, 3, 4, 5}, "}{", "1}{2}{3}{4}{5"},
+		{[]uint16{1, 2}, ",", "1,2"},
+		{[]uint16{1}, ",", "1"},
+		{[]uint16{}, ",", ""},
+	}
+	for _, c := range cases {
+		got := JoinUint16s(c.in, c.sep)
+		if got != c.out {
+			t.Errorf("JoinUint16s(%v, %q) == %q, out %q", c.in, c.sep, got, c.out)
+		}
+	}
+}
+
+func TestJoinUint32s(t *testing.T) {
+	cases := []struct {
+		in       []uint32
+		sep, out string
+	}{
+		{[]uint32{1, 2, 3, 4, 5}, "", "12345"},
+		{[]uint32{1, 2, 3, 4, 5}, ",", "1,2,3,4,5"},
+		{[]uint32{1, 2, 3, 4, 5}, "}{", "1}{2}{3}{4}{5"},
+		{[]uint32{1, 2}, ",", "1,2"},
+		{[]uint32{1}, ",", "1"},
+		{[]uint32{}, ",", ""},
+	}
+	for _, c := range cases {
+		got := JoinUint32s(c.in, c.sep)
+		if got != c.out {
+			t.Errorf("JoinUint32s(%v, %q) == %q, out %q", c.in, c.sep, got, c.out)
+		}
+	}
+}
+
+func TestJoinUint64s(t *testing.T) {
+	cases := []struct {
+		in       []uint64
+		sep, out string
+	}{
+		{[]uint64{1, 2, 3, 4, 5}, "", "12345"},
+		{[]uint64{1, 2, 3, 4, 5}, ",", "1,2,3,4,5"},
+		{[]uint64{1, 2, 3, 4, 5}, "}{", "1}{2}{3}{4}{5"},
+		{[]uint64{1, 2}, ",", "1,2"},
+		{[]uint64{1}, ",", "1"},
+		{[]uint64{}, ",", ""},
+	}
+	for _, c := range cases {
+		got := JoinUint64s(c.in, c.sep)
+		if got != c.out {
+			t.Errorf("JoinUint64s(%v, %q) == %q, out %q", c.in, c.sep, got, c.out)
+		}
+	}
+}

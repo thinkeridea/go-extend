64bce008d6ad928bb8aaa61443aab8a149f7c342
qiyin <qiyin@thinkeridea.com>
2019-11-04 12:22:39 +0800

    完善源文件版权声明及 exutf8 包文档

--- exunicode/exutf8/doc.go
+++ exunicode/exutf8/doc.go
@@ -13,27 +13,5 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-package benchmark
-
-import (
-	"testing"
-
-	"github.com/thinkeridea/go-extend/exstrings"
-)
-
-func BenchmarkStandardLibraryStringToBytes(b *testing.B) {
-	s := "Go语言是Google开发的一种静态强类型、编译型、并发型，并具有垃圾回收功能的编程语言。为了方便搜索和识别，有时会将其称为Golang。"
-	for i := 0; i < b.N; i++ {
-		p := []byte(s)
-		_ = p
-
-	}
-}
-
-func BenchmarkExstringsStringToBytes(b *testing.B) {
-	s := "Go语言是Google开发的一种静态强类型、编译型、并发型，并具有垃圾回收功能的编程语言。为了方便搜索和识别，有时会将其称为Golang。"
-	for i := 0; i < b.N; i++ {
-		p := exstrings.Bytes(s)
-		_ = p
-	}
-}
+// Package exutf8 用于改善标准库 utf8 包，提升操作多字节字符的性能及便利性，避免转换 []rune 类型。
+package exutf8

e1d4bdd06633e51cce508660347c682f3984bfd4
qiyin <qiyin@thinkeridea.com>
2021-01-08 10:33:40 +0800

    简化获取客户端ip逻辑

--- exnet/ip.go
+++ exnet/ip.go
@@ -43,8 +43,7 @@ func HasLocalIP(ip net.IP) bool {
 // ClientIP 尽最大努力实现获取客户端 IP 的算法。
 // 解析 X-Real-IP 和 X-Forwarded-For 以便于反向代理（nginx 或 haproxy）可以正常工作。
 func ClientIP(r *http.Request) string {
-	xForwardedFor := r.Header.Get("X-Forwarded-For")
-	ip := strings.TrimSpace(strings.Split(xForwardedFor, ",")[0])
+	ip := strings.TrimSpace(strings.Split(r.Header.Get("X-Forwarded-For"), ",")[0])
 	if ip != "" {
 		return ip
 	}
@@ -66,21 +65,17 @@ func ClientIP(r *http.Request) string {
 func ClientPublicIP(r *http.Request) string {
 	var ip string
 	for _, ip = range strings.Split(r.Header.Get("X-Forwarded-For"), ",") {
-		ip = strings.TrimSpace(ip)
-		if ip != "" && !HasLocalIPAddr(ip) {
+		if ip = strings.TrimSpace(ip); ip != "" && !HasLocalIPAddr(ip) {
 			return ip
 		}
 	}
 
-	ip = strings.TrimSpace(r.Header.Get("X-Real-Ip"))
-	if ip != "" && !HasLocalIPAddr(ip) {
+	if ip = strings.TrimSpace(r.Header.Get("X-Real-Ip")); ip != "" && !HasLocalIPAddr(ip) {
 		return ip
 	}
 
-	if ip, _, err := net.SplitHostPort(strings.TrimSpace(r.RemoteAddr)); err == nil {
-		if !HasLocalIPAddr(ip) {
-			return ip
-		}
+	if ip = RemoteIP(r); !HasLocalIPAddr(ip) {
+		return ip
 	}
 
 	return ""
@@ -88,7 +83,7 @@ func ClientPublicIP(r *http.Request) string {
 
 // RemoteIP 通过 RemoteAddr 获取 IP 地址， 只是一个快速解析方法。
 func RemoteIP(r *http.Request) string {
-	ip, _, _ := net.SplitHostPort(strings.TrimSpace(r.RemoteAddr))
+	ip, _, _ := net.SplitHostPort(r.RemoteAddr)
 	return ip
 }
 

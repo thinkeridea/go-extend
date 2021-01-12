b27a4fe10fe8923c84b0e626bc295133ed5ad364
Qi Yin <qiyin@thinkeridea.com>
2021-01-12 23:15:53 +0800

    exmath.Round 精度为负数时结算精准度问题 (#15)

--- exmath/benchmark/round_test.go
+++ exmath/benchmark/round_test.go
@@ -7,8 +7,19 @@ import (
 )
 
 func BenchmarkRound(b *testing.B) {
-	f := 0.15807659924030304
 	for i := 0; i < b.N; i++ {
-		_ = exmath.Round(f, 5)
+		_ = exmath.Round(169543.34596, 0)
+	}
+}
+
+func BenchmarkRoundDecimal(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = exmath.Round(0.15807659924030304, 5)
+	}
+}
+
+func BenchmarkRoundInteger(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = exmath.Round(169543.34596, -5)
 	}
 }
--- exmath/round.go
+++ exmath/round.go
@@ -10,6 +10,14 @@ import (
 // Round 四舍五入，ROUND_HALF_UP 模式实现
 // 返回将 val 根据指定精度 precision（十进制小数点后数字的数目）进行四舍五入的结果。precision 也可以是负数或零。
 func Round(val float64, precision int) float64 {
+	if precision == 0 {
+		return math.Round(val)
+	}
+
 	p := math.Pow10(precision)
+	if precision < 0 {
+		return math.Floor(val*p+0.5) * math.Pow10(-precision)
+	}
+
 	return math.Floor(val*p+0.5) / p
 }

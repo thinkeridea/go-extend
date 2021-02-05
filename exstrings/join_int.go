774c519f4f6abb8fa480e726e6166f03b1fa9b5e
qiyin <qiyin@thinkeridea.com>
2021-02-05 12:25:35 +0800

    简化join int系列方法，采用动态容量bytes.Buffer池

--- exstrings/join_int.go
+++ exstrings/join_int.go
@@ -9,144 +9,87 @@ import (
 	"github.com/thinkeridea/go-extend/pool"
 )
 
-var buffPool = pool.GetBuff64()
+var buffPool = pool.NewBuffer(64)
 
 // JoinInts 使用 sep 连接 []int 并返回连接的字符串
-func JoinInts(i []int, sep string) string {
-	buf := buffPool.Get()
-	for _, v := range i {
-		if buf.Len() > 0 {
-			buf.WriteString(sep)
-		}
-		buf.WriteString(strconv.FormatInt(int64(v), 10))
-	}
-
-	buffPool.Put(buf)
-	return buf.String()
+func JoinInts(v []int, sep string) string {
+	return joinInts(len(v), sep, func(i int) string {
+		return strconv.FormatInt(int64(v[i]), 10)
+	})
 }
 
 // JoinInt8s 使用 sep 连接 []int8 并返回连接的字符串
-func JoinInt8s(i []int8, sep string) string {
-	buf := buffPool.Get()
-	for _, v := range i {
-		if buf.Len() > 0 {
-			buf.WriteString(sep)
-		}
-		buf.WriteString(strconv.FormatInt(int64(v), 10))
-	}
-
-	buffPool.Put(buf)
-	return buf.String()
+func JoinInt8s(v []int8, sep string) string {
+	return joinInts(len(v), sep, func(i int) string {
+		return strconv.FormatInt(int64(v[i]), 10)
+	})
 }
 
 // JoinInt16s 使用 sep 连接 []int16 并返回连接的字符串
-func JoinInt16s(i []int16, sep string) string {
-	buf := buffPool.Get()
-	for _, v := range i {
-		if buf.Len() > 0 {
-			buf.WriteString(sep)
-		}
-		buf.WriteString(strconv.FormatInt(int64(v), 10))
-	}
-
-	buffPool.Put(buf)
-	return buf.String()
+func JoinInt16s(v []int16, sep string) string {
+	return joinInts(len(v), sep, func(i int) string {
+		return strconv.FormatInt(int64(v[i]), 10)
+	})
 }
 
 // JoinInt32s 使用 sep 连接 []int32 并返回连接的字符串
-func JoinInt32s(i []int32, sep string) string {
-	buf := buffPool.Get()
-	for _, v := range i {
-		if buf.Len() > 0 {
-			buf.WriteString(sep)
-		}
-		buf.WriteString(strconv.FormatInt(int64(v), 10))
-	}
-
-	buffPool.Put(buf)
-	return buf.String()
+func JoinInt32s(v []int32, sep string) string {
+	return joinInts(len(v), sep, func(i int) string {
+		return strconv.FormatInt(int64(v[i]), 10)
+	})
 }
 
 // JoinInt64s 使用 sep 连接 []int64 并返回连接的字符串
-func JoinInt64s(i []int64, sep string) string {
-	buf := buffPool.Get()
-	for _, v := range i {
-		if buf.Len() > 0 {
-			buf.WriteString(sep)
-		}
-		buf.WriteString(strconv.FormatInt(v, 10))
-	}
-
-	buffPool.Put(buf)
-	return buf.String()
+func JoinInt64s(v []int64, sep string) string {
+	return joinInts(len(v), sep, func(i int) string {
+		return strconv.FormatInt(v[i], 10)
+	})
 }
 
 // JoinUints 使用 sep 连接 []uint 并返回连接的字符串
-func JoinUints(i []uint, sep string) string {
-	buf := buffPool.Get()
-	for _, v := range i {
-		if buf.Len() > 0 {
-			buf.WriteString(sep)
-		}
-		buf.WriteString(strconv.FormatUint(uint64(v), 10))
-	}
-
-	buffPool.Put(buf)
-	return buf.String()
+func JoinUints(v []uint, sep string) string {
+	return joinInts(len(v), sep, func(i int) string {
+		return strconv.FormatUint(uint64(v[i]), 10)
+	})
 }
 
 // JoinUint8s 使用 sep 连接 []uint8 并返回连接的字符串
-func JoinUint8s(i []uint8, sep string) string {
-	buf := buffPool.Get()
-	for _, v := range i {
-		if buf.Len() > 0 {
-			buf.WriteString(sep)
-		}
-		buf.WriteString(strconv.FormatUint(uint64(v), 10))
-	}
-
-	buffPool.Put(buf)
-	return buf.String()
+func JoinUint8s(v []uint8, sep string) string {
+	return joinInts(len(v), sep, func(i int) string {
+		return strconv.FormatUint(uint64(v[i]), 10)
+	})
 }
 
 // JoinUint16s 使用 sep 连接 []uint16 并返回连接的字符串
-func JoinUint16s(i []uint16, sep string) string {
-	buf := buffPool.Get()
-	for _, v := range i {
-		if buf.Len() > 0 {
-			buf.WriteString(sep)
-		}
-		buf.WriteString(strconv.FormatUint(uint64(v), 10))
-	}
-
-	buffPool.Put(buf)
-	return buf.String()
+func JoinUint16s(v []uint16, sep string) string {
+	return joinInts(len(v), sep, func(i int) string {
+		return strconv.FormatUint(uint64(v[i]), 10)
+	})
 }
 
 // JoinUint32s 使用 sep 连接 []uint32 并返回连接的字符串
-func JoinUint32s(i []uint32, sep string) string {
-	buf := buffPool.Get()
-	for _, v := range i {
-		if buf.Len() > 0 {
-			buf.WriteString(sep)
-		}
-		buf.WriteString(strconv.FormatUint(uint64(v), 10))
-	}
-
-	buffPool.Put(buf)
-	return buf.String()
+func JoinUint32s(v []uint32, sep string) string {
+	return joinInts(len(v), sep, func(i int) string {
+		return strconv.FormatUint(uint64(v[i]), 10)
+	})
 }
 
 // JoinUint64s 使用 sep 连接 []uint64 并返回连接的字符串
-func JoinUint64s(i []uint64, sep string) string {
+func JoinUint64s(v []uint64, sep string) string {
+	return joinInts(len(v), sep, func(i int) string {
+		return strconv.FormatUint(v[i], 10)
+	})
+}
+
+func joinInts(n int, sep string, f func(i int) string) string {
 	buf := buffPool.Get()
-	for _, v := range i {
+	defer buffPool.Put(buf)
+	for i := 0; i < n; i++ {
 		if buf.Len() > 0 {
 			buf.WriteString(sep)
 		}
-		buf.WriteString(strconv.FormatUint(v, 10))
+		buf.WriteString(f(i))
 	}
 
-	buffPool.Put(buf)
 	return buf.String()
 }

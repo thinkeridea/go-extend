15790ea7943001da64a2f422dcbedaa1c44952e7
xiaoxinmiao <xiaoxm_001@163.com>
2022-11-01 08:26:02 +0800

    优化Round

--- exmath/round.go
+++ exmath/round.go
@@ -15,9 +15,6 @@ func Round(val float64, precision int) float64 {
 	}
 
 	p := math.Pow10(precision)
-	if precision < 0 {
-		return math.Floor(val*p+0.5) * math.Pow10(-precision)
-	}
 
 	return math.Floor(val*p+0.5) / p
 }
